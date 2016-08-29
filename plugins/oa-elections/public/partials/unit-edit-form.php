cd7429bb753c7b3400f30d2cb0d6fffe713f00ae
Kevin McKernan <kevin@mckernan.in>
2016-08-28 20:20:01 -0600

    Code formatting, button text on election form

--- plugins/oa-elections/public/partials/unit-edit-form.php
+++ plugins/oa-elections/public/partials/unit-edit-form.php
@@ -17,7 +17,7 @@ if ( is_page( 'request-an-election' ) && is_user_logged_in() ) {
 
 	if ( 'oae_election' !== $post_type ) {
 		$metabox_form_options = array(
-			'save_button' => 'Submit Election',
+			'save_button' => 'Request Election',
 		);
 		$form_action = 'create';
 	} else {
@@ -39,19 +39,19 @@ if ( is_page( 'request-an-election' ) && is_user_logged_in() ) {
 	if ( isset( $_GET['update'] ) ) {
 		// Get submitter's name
 		$name = get_post_meta( $post->ID, 'submitted_author_name', 1 );
-		$name = $name ? ' '. $name : '';
+		$name = $name ? ' ' . $name : '';
 		// Add notice of submission to our output
-		echo '<h3>' . sprintf( __( 'Thank you%s, your election has been updated. You will be notified by email once your election has been scheduled.', 'OA-Election' ), esc_html( $name ) ) . '</h3><br />';
+		echo '<h3>' . sprintf( esc_html( 'Thank you%s, your election has been updated. You will be notified by email once your election has been scheduled.', 'OA-Election' ), esc_html( $name ) ) . '</h3><br />';
 	}
 
 	if ( isset( $_GET['new_election'] ) ) {
 		// Get submitter's name
 		$name = get_post_meta( $post->ID, 'submitted_author_name', 1 );
-		$name = $name ? ' '. $name : '';
+		$name = $name ? ' ' . $name : '';
 		// Add notice of submission to our output
-		echo '<h3>' . sprintf( __( 'Thank you%s, your election has been submitted. You will be notified by email once your election has been scheduled.', 'OA-Election' ), esc_html( $name ) ) . '</h3><br />';
+		echo '<h3>' . sprintf( esc_html( 'Thank you%s, your election has been submitted. You will be notified by email once your election has been scheduled.', 'OA-Election' ), esc_html( $name ) ) . '</h3><br />';
 	}
 
-	echo '<h2>' . $election->meta_box['title'] . '</h2>';
-	echo cmb2_get_metabox_form( $election, 'unit_fields' );
+	echo '<h2>' . esc_html( $election->meta_box['title'] ) . '</h2>';
+	echo cmb2_get_metabox_form( $election, 'unit_fields', $metabox_form_options );
 }
