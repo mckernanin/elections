ef268679172f17958a7b3fa58b1e969b2bba5200
Kevin McKernan <kevin@mckernan.in>
2016-09-04 10:30:56 -0600

    Update election list to query by chapter

--- plugins/oa-elections/public/partials/election-list.php
+++ plugins/oa-elections/public/partials/election-list.php
@@ -1,13 +1,18 @@
 <?php
 if ( ! is_user_logged_in() ) {
-	$message = 'You must be logged in to view this page.';
-	echo $message;
+	echo 'You must be logged in to view this page.';
 } else {
 
 	$args = array(
 		'post_type' => 'oae_election',
 	);
+	if ( current_user_can( 'chapter-admin' ) ) {
+		$chapter_id = current( get_user_meta( get_current_user_id(), '_oa_election_user_chapter' ) );
+		$chapter = get_term( $chapter_id );
+		$args['oae_chapter'] = $chapter->slug;
+	}
 	$elections = new WP_Query( $args );
+	if ( $elections->have_posts() ) {
 	?>
 	<table>
 		<tr>
@@ -28,12 +33,15 @@ if ( ! is_user_logged_in() ) {
 					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 				</td>
 				<td>
-					<?php echo OAE_Util::get_chapter( get_the_id() ); ?>
+					<?php echo esc_html( OAE_Util::get_chapter( get_the_id() ) ); ?>
 				</td>
 				<td>
-					<?php echo OAE_Util::get_status( get_the_id() ); ?>
+					<?php echo esc_html( OAE_Util::get_status( get_the_id() ) ); ?>
 				</td>
 			</tr>
 		<?php } ?>
 	</table>
-<?php }
+<?php } else {
+	echo 'There are currently no elections for ' . $chapter->name;
+	}
+}
