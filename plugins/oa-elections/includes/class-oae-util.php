8099248d739767c897a2543c18ebd992670dbf9f
Kevin McKernan <kevin@mckernan.in>
2017-02-09 11:13:13 -0700

    more work on #3, check for false before current()

--- plugins/oa-elections/includes/class-oae-util.php
+++ plugins/oa-elections/includes/class-oae-util.php
@@ -10,44 +10,44 @@ class OAE_Util {
 			$post_id = get_the_id();
 		}
 
-		$term = current( get_the_terms( $post_id, 'oae_status' ) );
+		$term = get_the_terms( $post_id, 'oae_status' );
 		if ( ! $term ) {
 			return 'No status defined';
 		}
-		return $term->name;
+		return current( $term->name );
 	}
 
 	static function get_cand_status( $post_id = null ) {
 		if ( null === $post_id ) {
 			$post_id = get_the_id();
 		}
-		$term = current( get_the_terms( $post_id, 'oae_cand_status' ) );
+		$term = get_the_terms( $post_id, 'oae_cand_status' );
 		if ( ! $term ) {
 			return 'No status defined';
 		}
-		return $term->name;
+		return current( $term->name );
 	}
 
 	static function get_chapter( $post_id = null ) {
 		if ( null === $post_id ) {
 			$post_id = get_the_id();
 		}
-		$term = current( get_the_terms( $post_id, 'oae_chapter' ) );
+		$term = get_the_terms( $post_id, 'oae_chapter' );
 		if ( ! $term ) {
 			return 'No chapter defined';
 		}
-		return $term->name;
+		return current( $term->name );
 	}
 
 	static function get_chapter_term( $post_id = null ) {
 		if ( null === $post_id ) {
 			$post_id = get_the_id();
 		}
-		$term = current( get_the_terms( $post_id, 'oae_chapter' ) );
+		$term = get_the_terms( $post_id, 'oae_chapter' );
 		if ( ! $term ) {
 			return 'No chapter defined';
 		}
-		return $term;
+		return current( $term );
 	}
 
 	static function get_user_chapter( $user_id ) {
