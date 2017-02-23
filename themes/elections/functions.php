1ca22874b043110e268331fa9e8bddad0f0b6e04
Kevin McKernan <kevin@mckernan.in>
2017-02-23 14:40:42 -0700

    fix calendar not showing

--- themes/elections/functions.php
+++ themes/elections/functions.php
@@ -65,8 +65,8 @@ class ElectionTheme {
 		add_theme_support( 'soil-clean-up' );
 		add_theme_support( 'soil-disable-asset-versioning' );
 		add_theme_support( 'soil-disable-trackbacks' );
-		add_theme_support( 'soil-jquery-cdn' );
-		add_theme_support( 'soil-js-to-footer' );
+		// add_theme_support( 'soil-jquery-cdn' );
+		// add_theme_support( 'soil-js-to-footer' );
 		add_theme_support( 'soil-nav-walker' );
 		add_theme_support( 'soil-nice-search' );
 		add_theme_support( 'soil-relative-urls' );
