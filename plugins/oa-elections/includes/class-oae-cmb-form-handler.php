3f918b4d80693535c49c7e722112ae91553ea578
Kevin McKernan <kevin@mckernan.in>
2017-02-27 08:28:06 -0700

    fix candidate form handler, so candidates are put in the proper chapter

--- plugins/oa-elections/includes/class-oae-cmb-form-handler.php
+++ plugins/oa-elections/includes/class-oae-cmb-form-handler.php
@@ -242,7 +242,7 @@ class OAE_CMB_Form_Handler {
 			unset( $args['update'] );
 			$chapter = OAE_Util::get_chapter_term( $this->election_id );
 			wp_set_object_terms( $this->post_id, 'eligible', 'oae_cand_status' );
-			wp_set_object_terms( $this->post_id, absint( $chapter->id ), 'oae_chapter' );
+			wp_set_object_terms( $this->post_id, absint( $chapter->term_id ), 'oae_chapter' );
 			do_action( 'candidate_save', $this->post_id );
 		}
 
