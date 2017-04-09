bbde30360b9f092f7d87c223f7f4ba1bccaf981f
Kevin McKernan <kevin@mckernan.in>
2017-04-09 17:45:00 -0600

    fix required address field on nominations

--- themes/elections/assets/js/custom/form-validation.js
+++ themes/elections/assets/js/custom/form-validation.js
@@ -1,5 +1,6 @@
 $('#_oa_election_unit_location').attr('required', true);
 $('#_oa_candidate_address_address_2').attr('required', false);
+$('#_oa_nomination_address_address_2').attr('required', false);
 $('#_oa_election_unit_location').on('change', function() {
 	var location = $(this).val();
 	$('#_oa_election_unit_address_text').val(location);
